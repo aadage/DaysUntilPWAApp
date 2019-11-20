9790cbb3a9781c9137b68355f7e70842450e3b44
aadage <adage42@protonmail.com>
2019-11-19 19:40:33 -0500

    pwa troubleshooting

--- serviceworker.js
+++ serviceworker.js
@@ -96,3 +96,13 @@ self.addEventListener('activate', function (event) {
     //);
 });
 
+
+self.addEventListener('beforeinstallprompt', (e) => {
+    console.log('sw beforeinstallprompt')
+    e.prompt();
+  // Stash the event so it can be triggered later.
+  //deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  //showInstallPromotion();
+});
+
