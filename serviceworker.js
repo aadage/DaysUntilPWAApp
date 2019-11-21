123fa13f519f82ff6b5a6885b220863d0fc2922d
aadage <adage42@protonmail.com>
2019-11-20 21:21:04 -0500

    updated for publish

--- serviceworker.js
+++ serviceworker.js
@@ -96,13 +96,11 @@ self.addEventListener('activate', function (event) {
     //);
 });
 
-
 self.addEventListener('beforeinstallprompt', (e) => {
     console.log('sw beforeinstallprompt')
     e.prompt();
-  // Stash the event so it can be triggered later.
-  //deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  //showInstallPromotion();
+    // Stash the event so it can be triggered later.
+    //deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    //showInstallPromotion();
 });
-
